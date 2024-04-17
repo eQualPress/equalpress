00f5e82584bb0bc720a1793924ce74615e71feb7
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-17 13:37:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ then
         rm public/assets/env/config.json
         touch public/assets/env/config.json
         ./equal.run --get=envinfo-temp > public/assets/env/config.json
-        sleep 1
+        echo > "qsdqs"
         if [ -f public/assets/env/config.json ]; then
             sed -i 's#"backend_url": *"\(.*\)"#"backend_url": "\1\\/equal.php"#' public/assets/env/config.json
         else
